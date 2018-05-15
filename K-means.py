b5ee7e523f1d9429502273a51b4c1b0f1d6b1e07
aw10279 <123QWEasd>
2018-05-16 07:32:54 +0800

    K-means with sklearn

--- K-means.py
+++ K-means.py
@@ -7,7 +7,7 @@ from sklearn import metrics #聚类效果评估工具
 data, lable = make_blobs(n_samples=1000, n_features=2, centers=[[-2,-2], [0,0], [2,2],[5,5]], cluster_std=[0.4,0.5,0.3,0.4], random_state=15)
 #random_state用法同random seed，即随机种子，用来制约随机数的生成。只要该参数固定，生成的随机数会完全一样。
 #x保存数据集，y保存标签（分成几类就有几种标签）
-#因需要数据可视化，n_features只取2（二维数据），实际可处理多维数据，同时centers不能写坐标，只写个数（写了坐标就默认为二维数据）
+#因需要数据可视化，n_features只取2（二维数据），实际可处理多维数据，同时centers必须写多维坐标或只写个数（写二维坐标就默认为二维数据）
 
 plt.scatter(data[:,0], data[:,1],marker="o")
 #x[:,0]就是取所有行的第0个数据, x[:,1] 就是取所有行的第1个数据,是numpy多维数组的一种写法。
